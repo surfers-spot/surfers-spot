e397befa0bb1a4eb5e3b136461197f2267120693
blincoln15 <41168094+blincoln15@users.noreply.github.com>
2021-11-30 10:10:30 -1000

    updated go to a random break in footer
    added navlink functionality for footer random break

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -11,7 +11,7 @@ export default class FooterMenu extends React.Component {
             <List>
               <text style={{ color: 'grey', fontSize: 22, fontFamily: 'Volkhov' }}>Navigate</text><hr/>
               <List.Item as={NavLink} style={{ color: 'white' }} exact to="/about" key='about'>About Us</List.Item>
-              <List.Item>Go to a Random Break</List.Item>
+              <List.Item as={NavLink} style={{ color: 'white' }} exact to="/random" key='random'>Go to a Random Break</List.Item>
               <List.Item as={NavLink} style={{ color: 'white' }} exact to="/bowls" key='bowls'>Visit a Popular Break</List.Item>
               <List.Item>Break Directory</List.Item>
               <List.Item>Leave a Review</List.Item>
