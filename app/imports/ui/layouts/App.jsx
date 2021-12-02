89916a6b856478664a68ca19aed44c2cf010aa6a
Aaron Thomas <at49@hawaii.edu>
2021-12-01 23:40:10 -1000

    Removed all "Stuff" related items.

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -7,11 +7,7 @@ import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import Landing from '../pages/Landing';
-import ListStuff from '../pages/ListStuff';
-import ListStuffAdmin from '../pages/ListStuffAdmin';
-import AddStuff from '../pages/AddStuff';
 import AboutUs from '../pages/AboutUs';
-import EditStuff from '../pages/EditStuff';
 import NotFound from '../pages/NotFound';
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
@@ -39,11 +35,7 @@ class App extends React.Component {
             <Route path="/canoes" component={Canoes}/>
             <Route path="/kewalos" component={Kewalos}/>
             <Route path="/random" component={Random}/>
-            <ProtectedRoute path="/list" component={ListStuff}/>
-            <ProtectedRoute path="/add" component={AddStuff}/>
             <Route path="/addBreak" component={AddBreak}/>
-            <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
-            <AdminProtectedRoute path="/admin" component={ListStuffAdmin}/>
             <Route component={NotFound}/>
           </Switch>
           <Footer/>
