3f0a769f2df5b33f5a3db5f4e8ca35c21bfff3a1
blincoln15 <41168094+blincoln15@users.noreply.github.com>
2021-12-15 15:10:36 -1000

    Finished barebones Review Collection
    Has name associated with break
    Has text for actual review message

---
+++ app/imports/api/review/Review.js
@@ -0,0 +1,31 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/**
+ * The BreaksCollection. It encapsulates state and variable values for break.
+ */
+class ReviewCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'ReviewCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      name: { type: String, index: true, unique: true },
+      text: String,
+    }, { tracker: Tracker });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+/**
+ * The singleton instance of the BreaksCollection.
+ * @type {ReviewCollection}
+ */
+export const Breaks = new ReviewCollection();
