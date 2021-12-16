577256a9dd99662316901eb71d5c04b915d1b2b2
Sync925 <michaellavers1@gmail.com>
2021-12-15 20:23:07 -1000

    Update Footer.jsx

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -36,7 +36,7 @@ export default class FooterMenu extends React.Component {
               <List.Item>Want to be informed when new breaks are added?</List.Item>
               <List.Item>Enter your email below to stay informed</List.Item>
               <List.Item>
-                <AddMail name="mailing list"/>
+                <AddMail name="general mailing"/>
               </List.Item>
             </List>
           </Grid.Column>
